bd3d675d45dbcc59e0b0e0400b6bcdf8e7a0a1e2
Shinya Takahashi <s.takahashi313@gmail.com>
2017-06-19 14:56:38 +0900

    Coerce removing err effect

--- src/Node/Stdout.purs
+++ src/Node/Stdout.purs
@@ -3,12 +3,12 @@ module Node.Stdout (log) where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Eff.Exception (EXCEPTION)
 import Node.Encoding (Encoding(UTF8))
 import Node.Process (stdout)
 import Node.Stream (writeString)
+import Unsafe.Coerce (unsafeCoerce)
 
 
 
-log :: forall e. String -> Eff (console :: CONSOLE, err :: EXCEPTION | e) Unit
-log str = void $ writeString stdout UTF8 (str <> "\n") $ pure unit
+log :: forall e. String -> Eff (console :: CONSOLE | e) Unit
+log str = unsafeCoerce $ void $ writeString stdout UTF8 (str <> "\n") $ pure unit
